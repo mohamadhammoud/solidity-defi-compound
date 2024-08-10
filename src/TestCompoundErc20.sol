2293499d2e5d2e7ed5728c90460827d4508fe328
Mohamad Hammoud <mohamad.hammoudk@gmail.com>
2024-08-10 19:18:27 +0300

    💥 added redeem methods

--- src/TestCompoundErc20.sol
+++ src/TestCompoundErc20.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./interfaces/ICompound.sol";
 
 /// @title TestCompoundErc20 Contract
@@ -9,7 +9,7 @@ import "./interfaces/ICompound.sol";
 /// @dev The contract interacts with a specific ERC20 token and its corresponding cToken on the Compound protocol.
 contract TestCompoundErc20 {
     /// @notice The underlying ERC20 token that will be supplied to the Compound protocol
-    IERC20 public token;
+    ERC20 public token;
 
     /// @notice The cToken representing the supplied ERC20 token on the Compound protocol
     CErc20 public cToken;
@@ -17,11 +17,14 @@ contract TestCompoundErc20 {
     /// @dev Error to indicate that minting cTokens failed
     error MintFailed();
 
+    /// @dev Error to indicate that redeeming cTokens failed
+    error RedeemFailed();
+
     /// @notice Constructor to initialize the contract with the ERC20 token and corresponding cToken addresses
     /// @param _token The address of the ERC20 token that will be supplied (e.g., DAI)
     /// @param _cToken The address of the corresponding cToken on the Compound protocol (e.g., cDAI)
     constructor(address _token, address _cToken) {
-        token = IERC20(_token);
+        token = ERC20(_token);
         cToken = CErc20(_cToken);
     }
 
@@ -69,5 +72,36 @@ contract TestCompoundErc20 {
 
         // Get the current supply rate per block from the cToken
         supplyRatePerBlock = cToken.supplyRatePerBlock();
+    }
+
+    /// @notice Estimate the current balance of the underlying asset held by this contract in terms of the ERC20 token
+    /// @dev This function calculates the underlying asset balance by multiplying the cToken balance by the current exchange rate and adjusting for token decimals
+    /// @return The estimated balance of the underlying asset in the smallest unit of the ERC20 token (e.g., if the token is DAI, this would be in wei)
+    function estimateBalanceOfUnderlying() external returns (uint256) {
+        // Get the cToken balance of this contract
+        uint256 cTokenbalance = cToken.balanceOf(address(this));
+
+        // Get the current exchange rate from cToken to the underlying asset
+        uint256 exchangeRate = cToken.exchangeRateCurrent();
+
+        // cToken typically has 8 decimals
+        uint256 cTokenDecimals = 8;
+
+        // Calculate and return the balance of the underlying asset, adjusting for decimals
+        return
+            (cTokenbalance * exchangeRate) /
+            10 ** (18 + token.decimals() - cTokenDecimals);
+    }
+
+    function redeem(uint _amount) external returns (uint) {
+        uint256 redeemResult = cToken.redeem(_amount);
+
+        // Revert the transaction if redeeming failed
+        if (redeemResult != 0) {
+            revert RedeemFailed();
+        }
+
+        // Return the result of the mint operation (0 indicates success)
+        return redeemResult;
     }
 }
